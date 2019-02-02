5ace7196b8ec01090308ec16fa877d65e240d5fa
Li Swafford <liswafford@yahoo.com>
2019-02-01 22:45:36 -0600

    add css

--- src/components/CharCard.js
+++ src/components/CharCard.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const CharCard = (props) =>{
   const { char } = props;
   return(
-    <div>
+    <div className="whole-card">
       <Link to={`/chardetails/${char.id}`}> 
         <h1>{char.name}</h1>
         <img className="card-img"src={`${char.thumbnail.path}.${char.thumbnail.extension}`} alt={char.name}/>
