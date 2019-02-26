5787a25223a48290c244276b54594f5c418a6136
Li Swafford <liswafford@yahoo.com>
2019-02-26 15:13:28 -0600

    add loading animation

--- src/components/CharDetails.js
+++ src/components/CharDetails.js
@@ -1,62 +1,79 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { getSingleChar, getComics } from '../ducks/reducer';
-import ComicModal from './ComicModal';
-
+import React, { Component } from "react";
+import ReactLoading from "react-loading";
+import { connect } from "react-redux";
+import { getSingleChar, getComics } from "../ducks/reducer";
+import ComicModal from "./ComicModal";
 
 class CharDetails extends Component {
   state = {
     clickedComic: false,
     selectedIndex: null
-  }
+  };
 
-  
-  componentDidMount(){
+  componentDidMount() {
     this.props.getSingleChar(this.props.match.params.id);
     this.props.getComics(this.props.match.params.id);
     // console.log(this.props)
   }
   render() {
-    console.log(this.state)
-    const {char, comics} = this.props;
-    const comicList = comics.map((comic,i)=>{
+    console.log(this.state);
+    const { char, comics } = this.props;
+    const comicList = comics.map((comic, i) => {
       return (
-        <div 
-        className="comic-card" 
-        key={comic.id}
-        onClick={() => this.setState({clickedComic: true, selectedIndex: i})}>
+        <div
+          className="comic-card"
+          key={comic.id}
+          onClick={() =>
+            this.setState({ clickedComic: true, selectedIndex: i })
+          }
+        >
           <h4>{comic.title}</h4>
           <div>
-            <img 
-            src={comic.thumbnail && `${comic.thumbnail.path}.${comic.thumbnail.extension}`} 
-            alt={comic.title}
+            <img
+              src={
+                comic.thumbnail &&
+                `${comic.thumbnail.path}.${comic.thumbnail.extension}`
+              }
+              alt={comic.title}
             />
           </div>
         </div>
-      )
-    })
+      );
+    });
     return (
       <div>
         <h2>{char.name}</h2>
-        <img 
+        <img
           className="card-img"
-          src={char.thumbnail && `${char.thumbnail.path}.${char.thumbnail.extension}`} 
+          src={
+            char.thumbnail &&
+            `${char.thumbnail.path}.${char.thumbnail.extension}`
+          }
           alt={char.name}
         />
         <p>{char.description}</p>
-        {this.props.loading 
-          ? <h2>Loading Comics</h2> 
-          : <div className="comics-wrapper">
-              {comicList}
-            </div>
-        }
-        {this.state.clickedComic && <ComicModal 
-          closeModal={()=>this.setState({clickedComic:false})}
-          comic={comics[this.state.selectedIndex]}
-        />}
+        {this.props.loading ? (
+          <ReactLoading
+            type="spinningBubbles"
+            color="blue"
+            height={"20vh"}
+            width={"20vw"}
+          />
+        ) : (
+          <div className="comics-wrapper">{comicList}</div>
+        )}
+        {this.state.clickedComic && (
+          <ComicModal
+            closeModal={() => this.setState({ clickedComic: false })}
+            comic={comics[this.state.selectedIndex]}
+          />
+        )}
       </div>
     );
   }
 }
 
-export default connect(state=>state,{getSingleChar, getComics})(CharDetails);
+export default connect(
+  state => state,
+  { getSingleChar, getComics }
+)(CharDetails);
